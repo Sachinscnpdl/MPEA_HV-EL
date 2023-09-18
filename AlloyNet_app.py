b329ab82c33ab0114e234d964e91132a7af5b60d
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2023-09-18 18:50:36 +0200

    Update AlloyNet_app.py

--- AlloyNet_app.py
+++ AlloyNet_app.py
@@ -81,13 +81,13 @@ if selected_tab == "New alloy design":
     # # Prediction Results!
     # """
     # Define the text style for hardness and elongation with values and units
-    hardness_style = "<h2 style='color:blue; font-size:24px;'>{} HV</h2>".format(hardness)
+    hardness_style = "<h2 style='color:green; font-size:24px;'>{} HV</h2>".format(hardness)
     elongation_style = "<h2 style='color:green; font-size:24px;'>{} %</h2>".format(elongation)
     
     # Display the styled text using st.markdown
-    st.markdown("<h2 style='color:blue;'>The hardness is:      </h2> {}".format(hardness_style), unsafe_allow_html=True)
-    st.markdown("<h2 style='color:green;'>The elongation is:</h2> {}".format(elongation_style), unsafe_allow_html=True)
-    st.markdown("<h2 style='color:blue;'>The hardness is:      </h2> {}", hardness, unsafe_allow_html=True)
+    st.markdown("<h2 style='color:blue;; font-size:24px'>The hardness is:      </h2> {}".format(hardness_style), unsafe_allow_html=True)
+    st.markdown("<h2 style='color:blue;; font-size:24px'>The elongation is:</h2> {}".format(elongation_style), unsafe_allow_html=True)
+
 
     
     # Display other content as needed
