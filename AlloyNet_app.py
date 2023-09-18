d8250e53bef72f12387d4c4d01d064c163548bea
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2023-09-18 19:46:36 +0200

    Update AlloyNet_app.py

--- AlloyNet_app.py
+++ AlloyNet_app.py
@@ -124,7 +124,7 @@ if selected_tab == "New alloy design":
     # Display the property names with left alignment and values rounded to 4 decimal places
     for i in range(len(property_names_latex)):
         formatted_value = "{:.3g}".format(values[i])  # Format value to 4 decimal places
-        st.latex(r"\noindent {} : {}".format(property_names_latex[i], formatted_value))
+        st.latex(r"\quad {} : {}".format(property_names_latex[i], formatted_value))
 
 
 
