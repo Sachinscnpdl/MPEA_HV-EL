34aeb715509a6cefe9018667d8c2d783ba18e986
Anil Kunwar <anilkunwar318@yahoo.com>
2023-09-19 15:06:41 +0200

    Update AlloyNet_app.py

--- AlloyNet_app.py
+++ AlloyNet_app.py
@@ -107,7 +107,7 @@ if selected_tab == "New alloy design":
     alloy_style = "<h2 style='color:green; font-size:24px;'>{} </h2>".format(selected_predefined_formula)
     manufacturing_style = "<h2 style='color:green; font-size:24px;'>{} </h2>".format(selected_fabrication_type)
     # Display the styled text using st.markdown on the same line
-    st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>Multi-principal element alloy:</h2> {}".format(alloy_style), unsafe_allow_html=True)
+    st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>Multi-principal Element Alloy:</h2> {}".format(alloy_style), unsafe_allow_html=True)
     st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>Fabrication Type:</h2> {}".format(manufacturing_style), unsafe_allow_html=True)
     #st.write("Multi-principal element alloy:", selected_predefined_formula)
     #st.write("Fabrication Type:", selected_fabrication_type)
@@ -119,8 +119,9 @@ if selected_tab == "New alloy design":
     elongation_style = "<h2 style='color:green; font-size:24px;'>{} %</h2>".format(elongation)
     
     # Display the styled text using st.markdown on the same line
-    st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>The hardness is:</h2> {}".format(hardness_style), unsafe_allow_html=True)
-    st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>The elongation is:</h2> {}".format(elongation_style), unsafe_allow_html=True)
+    st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>Hardness:</h2> {}".format(hardness_style), unsafe_allow_html=True)
+    st.markdown("<h2 style='color:blue; font-size:24px; display: inline;'>Ducitility:</h2> {}".format(elongation_style), unsafe_allow_html=True)
+
 
 
 
