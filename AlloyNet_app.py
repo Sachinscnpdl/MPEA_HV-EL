7c1dad20d4c38763c0800da3e68fdf9ac9e33217
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2023-09-19 15:00:25 +0200

    Update AlloyNet_app.py

--- AlloyNet_app.py
+++ AlloyNet_app.py
@@ -30,7 +30,28 @@ from app_functions import *
 # Using HTML formatting to add color to the font in st.header
 st.markdown('<h1 style="color:purple;">AlloyManufacturingNet </h1>', unsafe_allow_html=True)
 # Add a dropdown to select a pre-defined formula
-
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: "plots/logo.png";
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 import streamlit as st
 import pandas as pd
 
