d5fbfdfa44c55924898d805c72a480b5c14e9e24
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2024-06-13 00:53:58 +0200

    Update AlloyNet_app.py

--- AlloyNet_app.py
+++ AlloyNet_app.py
@@ -103,7 +103,7 @@ if selected_tab == "New alloy design":
             new_row = {'S.N': len(st.session_state.df_selected_formulas) + 1, 'Alloys': alloy_string, 'Fabrication_type': None}
             st.session_state.df_selected_formulas = st.session_state.df_selected_formulas.append(new_row, ignore_index=True)
             
-            st.write('Defined new alloy:', alloy_string)
+            # st.write('Defined new alloy:', alloy_string)
     
     # Display the DataFrame only if it has necessary columns
     if 'Alloys' in st.session_state.df_selected_formulas.columns and 'Fabrication_type' in st.session_state.df_selected_formulas.columns:
